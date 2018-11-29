399f3164b9c2cb5ab8eeafbe1b03fe6e960f25b3
Daniel-Alpha <45221469+Daniel-Alpha@users.noreply.github.com>
2018-11-29 10:35:35 +0100

    segun-onanuga test
    Thank you very much sir

---
+++ segun-onanuga test/Ex1.py
@@ -0,0 +1,7 @@
+# This is a function that takes in a string as an input and repeats that string in 3 places
+def join_string(name_1):
+    new_name = name_1 * 3
+    return new_name
+
+
+print(join_string("boy"))
---
+++ segun-onanuga test/Ex3.py
@@ -0,0 +1,7 @@
+#A function that gives back the first and last character of what was given
+def first_last_char(name_2):
+    first_char = name_2[0]
+    last_char = name_2[-1]
+    new_name = first_char + last_char
+    return print(new_name)
+first_last_char("Segun")
